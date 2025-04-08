8e3d1f3349deee33a81e35db0e027e79a26a11b5
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-04-08 15:17:20 -0300

    mend

--- back-end/src/main/java/com/gustavo/ecommerce/projeto/service/ProdutoService.java
+++ back-end/src/main/java/com/gustavo/ecommerce/projeto/service/ProdutoService.java
@@ -2,7 +2,6 @@ package com.gustavo.ecommerce.projeto.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
